6bc1023f590ca6209939ff9140e4507513e21722
Ryan Collingham <ryanc118@gmail.com>
2020-03-18 18:13:41 +0000

    Configure and log port number

--- pytest_ui_server/__main__.py
+++ pytest_ui_server/__main__.py
@@ -6,19 +6,22 @@ import sys
 from pytest_ui_server import api
 
 logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger(__name__)
 
 
 def main() -> int:
     """Start the server."""
     args = parse_args()
     app, socketio = api.build_app(args.directory)
-    socketio.run(app)
+    LOGGER.critical(f"View in your browser at http://localhost:{args.port}/")
+    socketio.run(app, port=args.port)
 
 
 def parse_args() -> argparse.Namespace:
     """Parse command-line args."""
     parser = argparse.ArgumentParser()
     parser.add_argument("directory", nargs="?", default=".")
+    parser.add_argument("-p", "--port", type=int, default=5000)
     return parser.parse_args()
 
 
