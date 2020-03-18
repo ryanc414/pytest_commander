d11fc530d7942dfa25a5e0458e3c2604a4f9e032
Ryan Collingham <ryanc118@gmail.com>
2020-03-18 19:04:02 +0000

    Load report skeleton in app

--- pytest_web_ui/src/App.tsx
+++ pytest_web_ui/src/App.tsx
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
+interface AppState {
+  resultTree: any,
+  loading: boolean,
+}
+
+/**
+ * Top level App component.
+ */
+class App extends React.Component<object, AppState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      resultTree: null,
+      loading: false,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true }, this.getResultTree);
+  }
+
+  getResultTree() {
+    axios.get("/api/v1/result-tree").then(response => {
+      this.setState({ resultTree: response.data, loading: false });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <NavColumn resultTree={this.state.resultTree} />
+        <InfoPane resultTree={this.state.resultTree} />
+      </div>
+    );
+  }
+}
+
+interface NavProps {
+  resultTree: any,
+}
+
+interface InfoPaneProps {
+  resultTree: any,
+}
+
+/**
+ * NavColumn component: renders the current navigation selection.
+ * @param props Component props
+ */
+const NavColumn = (props: NavProps) => {
+  if (!props.resultTree) {
+    return <span>Loading...</span>;
+  }
+
+  const childBranches = Object.keys(props.resultTree.child_branches);
+  const childLeaves = Object.keys(props.resultTree.child_leaves);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {childBranches.map((entry: string) => <div>{entry + " (branch)"}</div>)}
+      {childLeaves.map((entry: string) => <div>{entry + " (leaf)"}</div>)}
+    </>
   );
+}
+
+/**
+ * InfoPane component: renders information on the currently selected testcase (if any)
+ * @param props Component props
+ */
+const InfoPane = (props: InfoPaneProps) => {
+  return <span>InfoPane</span>
 }
 
 export default App;
