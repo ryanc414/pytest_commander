922ac95acfc91583a9b52e94849526679283c470
Ryan Collingham <ryanc118@gmail.com>
2020-03-31 15:55:51 +0100

    Add debug flag and help text to cmdline parser

--- pytest_ui_server/__main__.py
+++ pytest_ui_server/__main__.py
@@ -1,28 +1,49 @@
 """PyTest UI server main entry point."""
 import argparse
 import logging
+import os
 import sys
 
 from pytest_ui_server import api
 
-logging.basicConfig(level=logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
 
 
 def main() -> int:
     """Start the server."""
     args = parse_args()
+
+    log_level = logging.DEBUG if args.debug else logging.CRITICAL
+    logging.basicConfig(level=log_level)
+
     app, socketio = api.build_app(args.directory)
     LOGGER.critical(f"View in your browser at http://localhost:{args.port}/")
-    socketio.run(app, port=args.port)
+    socketio.run(app, port=args.port, debug=args.debug)
     return 0
 
 
 def parse_args() -> argparse.Namespace:
     """Parse command-line args."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("directory", nargs="?", default=".")
-    parser.add_argument("-p", "--port", type=int, default=5000)
+    parser.add_argument(
+        "directory",
+        nargs="?",
+        default=".",
+        help=f"Directory to find PyTest test modules, defaults to cwd ({os.getcwd()})",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=5000,
+        help="Port number to bind to, defaults to 5000",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="Enable debug logging and use the debug web server",
+    )
     return parser.parse_args()
 
 
